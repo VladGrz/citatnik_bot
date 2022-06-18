b1524471b077039efbdec4dde33693d5c6dbf29c
VladGrz <60891301+VladGrz@users.noreply.github.com>
2022-06-18 11:18:44 +0300

    More comments and fixed showing huge amount of citation in inline mode

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ from aiogram import executor
 from bot import dp
 
 if __name__ == '__main__':
+    # Starting bot
     executor.start_polling(dp, skip_updates=True)
--- loader.py
+++ loader.py
@@ -5,6 +5,11 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data import config
 
+# Defining basic Bot object through which we will connect to Telegram Api
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+
+# Defining storage to store info about citation in states
 storage = MemoryStorage()
+
+# Defining dispatcher which will process incoming updates
 dp = Dispatcher(bot, storage=storage)
